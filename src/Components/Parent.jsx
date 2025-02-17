9fc2ad9c9447d20468cf29535661a4bd0a119adf
Tirumala <84436332+20A91A04M5@users.noreply.github.com>
2025-02-17 18:59:21 +0530

    Update Parent.jsx
    add small functionality

--- src/Components/Parent.jsx
+++ src/Components/Parent.jsx
@@ -15,7 +15,7 @@ export default class Parent extends Component {
       <div>
         <h1>hello {this.state.name},{this.state.count}</h1>
         {console.log("hii this parent")}
-        <button onClick={()=>{this.setState({name:"tej"})}}>names</button>
+        <button onClick={()=>{this.setState({name:"tej"})}} onDoubleClick={()=>{this.setState({name:"React"})}}>names</button>
         <button disabled={this.state.count>=20} onClick={()=>{this.setState({count:this.state.count+1})}}>INC</button>
         <button disabled={this.state.count<=1} onClick={()=>{this.setState({count:this.state.count-1})}}>DEC</button>
         <Child prop={this.state.count} />
